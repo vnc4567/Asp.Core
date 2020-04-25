8c3067ca9f830e92117547d00afd395ee2c4f113
Vincent Benazet <benazet.vincent@gmail.com>
2020-04-25 10:34:14 +0200

    nlog file

---
+++ WebTest/Domain/Dollar/Dollar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Dollar
+{
+    public class Dollar
+    {
+        public Dollar(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        private decimal Amount {  get;  set; }
+        public Dollar Times(decimal multiplier)
+        {
+            return new Dollar(this.Amount * multiplier);
+        }
+        public override bool Equals(Object dollar)
+        {
+            return this.Amount==((Dollar) dollar).Amount;
+        }
+    }
+}
